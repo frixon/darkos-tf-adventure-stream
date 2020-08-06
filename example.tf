59a288d5e1bd0a27a3c6814a7c1cb58e7f76e43a
Joe Frixon <frixon@gmail.com>
2020-08-06 10:40:57 +0400

    output added
    the output of the public IP address added

--- example.tf
+++ example.tf
@@ -92,3 +92,10 @@ resource "aws_security_group" "allow_http" {
     Name = "allow_http"
   }
 }
+
+# --- output ---
+
+output "instance_ip_addr" {
+  value       = aws_instance.myTerraFormInstance.public_ip
+  description = "The private IP address of the main server instance."
+}
